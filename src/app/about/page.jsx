8bbcfc2acfffd28e716304a2f2a07dbc76cffd29
Heitor Chang <heitorpontual@gmail.com>
2023-05-22 18:46:08 -0300

    initial setup

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Button from '@mui/material/Button';
+
+export default function Page() {
+  return (
+    <div>
+    <h3>About Alexie-FE</h3>
+      <Button variant="outlined">Click me</Button>
+
+      <h3>Credits</h3>
+      <ul>
+        <li>Next.js</li>
+        <li>MUI</li>
+      </ul>
+    </div>
+  );
+}
